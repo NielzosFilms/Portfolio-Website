d6461b9753cddb2384436638c4928fb5e0fe771f
Niels Hazelaar <nielzosfilms@gmail.com>
2025-05-12 22:15:06 +0200

    Angular setup

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-card',
+    imports: [],
+    template: '<ng-content></ng-content>',
+    styleUrl: './card.component.scss',
+})
+export class CardComponent {
+
+}
